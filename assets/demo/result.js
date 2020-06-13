e72df198f6b1a2833f57ca3fb554c33568125acb
Sihoo-milk <>
2020-06-13 22:15:03 +0900

    ファイルの追加

---
+++ assets/demo/result.js
@@ -0,0 +1,86 @@
+//円グラフ（パイ型）
+var ctx = document.getElementById("chart-pie01");
+var myPieChart = new Chart(ctx, {
+  //グラフの種類
+  type: 'pie',
+  //データの設定
+  data: {
+      //データ項目のラベル
+      labels: ["Red", "Green", "Yellow"],
+      //データセット
+      datasets: [{
+          //背景色
+          backgroundColor: [
+              "#FF6384",
+              "#36A2EB",
+              "#FFCE56"
+          ],
+          //背景色(ホバーしたとき)
+          hoverBackgroundColor: [
+              "#FF6384",
+              "#36A2EB",
+              "#FFCE56"
+          ],
+          //グラフのデータ
+          data: [300, 50, 100]
+      }]
+  }
+});
+
+//円グラフ（パイ型）
+var ctx = document.getElementById("chart-pie02");
+var myPieChart = new Chart(ctx, {
+  //グラフの種類
+  type: 'pie',
+  //データの設定
+  data: {
+      //データ項目のラベル
+      labels: ["Red", "Green", "Yellow"],
+      //データセット
+      datasets: [{
+          //背景色
+          backgroundColor: [
+              "#FF6384",
+              "#36A2EB",
+              "#FFCE56"
+          ],
+          //背景色(ホバーしたとき)
+          hoverBackgroundColor: [
+              "#FF6384",
+              "#36A2EB",
+              "#FFCE56"
+          ],
+          //グラフのデータ
+          data: [300, 50, 100]
+      }]
+  }
+});
+
+//円グラフ（パイ型）
+var ctx = document.getElementById("chart-pie03");
+var myPieChart = new Chart(ctx, {
+  //グラフの種類
+  type: 'pie',
+  //データの設定
+  data: {
+      //データ項目のラベル
+      labels: ["Red", "Green", "Yellow"],
+      //データセット
+      datasets: [{
+          //背景色
+          backgroundColor: [
+              "#FF6384",
+              "#36A2EB",
+              "#FFCE56"
+          ],
+          //背景色(ホバーしたとき)
+          hoverBackgroundColor: [
+              "#FF6384",
+              "#36A2EB",
+              "#FFCE56"
+          ],
+          //グラフのデータ
+          data: [300, 50, 100]
+      }]
+  }
+});
